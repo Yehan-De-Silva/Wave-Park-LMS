2339d1e588c726ae0447c822d9605758b6e66922
Yehan De Silva <2002yehandesilva@gmail.com>
2025-03-07 18:57:25 +0530

    a

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <h1 style="color: black;">This is header</h1>
         <a href="authentication/Login" class="btn btn-primary">Login / Register</a>
         <a href="index" class="btn btn-primary">Home</a>
-        <a href="Lecturer/Lec_dashboard" class="btn btn-primary">Instructor Dashboard</a>
+        <a href="Lecturer/Lec_dashboard.php" class="btn btn-primary">Instructor Dashboard</a>
     </header>
     </div>
 
