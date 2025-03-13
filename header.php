d1dae2fb765cec735d62a1453429ee878f9738f6
praveen <praveenisuru06@gmail.com>
2025-03-13 07:43:05 +0530

    Home

--- header.php
+++ header.php
@@ -8,34 +8,33 @@
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    
-<<<<<<< HEAD
-    <div style="background-color: white;" class="px-5 py-2" >
-        <header style="display: flex; flex-direction: row; flex-direction: row; column-gap: 10px; row-gap: 10px;">
-            <div class="row w-100">
-                <div class="col-4">
-                    <img class="col-5" src="assets/images/logo.png"></div>
-                
-                <div class="col-5 menu-bar">
-                    <a href="index.php" class="menu-link">Home</a>
-                    <a href="course.php" class="menu-link">Course</a>
-                    <a href="about.php" class="menu-link">About</a>
-                    <a href="contact.php" class="menu-link">Contact Us</a>
+    <div class="head">
+        <div style="background-color: white;" >
+            <header style="display: flex; flex-direction: row; flex-direction: row; column-gap: 10px; row-gap: 10px;">
+                <div class="row w-100">
+                    <div class="col-4">
+                        <img class="col-5" src="assets/images/logo.png"></div>
+                    
+                    <div class="col-5 menu-bar">
+                        <a href="index.php" class="menu-link">Home</a>
+                        <a href="course.php" class="menu-link">Course</a>
+                        <a href="about.php" class="menu-link">About</a>
+                        <a href="contact.php" class="menu-link">Contact Us</a>
+                    </div>
+                    <div class="col-2 login-btns">
+                        <a href="authentication/Login" class="btn btn-primary">Login</a>
+                        <a href="authentication/Login" class="btn btn-primary">Register</a>
+                    </div>
                 </div>
-                <div class="col-2 login-btns">
-                    <a href="authentication/Login" class="btn btn-primary">Login</a>
-                    <a href="authentication/Login" class="btn btn-primary">Register</a>
-                </div>
-            </div>
+            </header>
+
+        
+        <header style="justify-content: center; align-items: center; align-self: center; display: flex; flex-direction: row; flex-direction: row; column-gap: 10px; row-gap: 10px;">
+        
         </header>
-=======
-    <div style="background-color: white; padding: 30px;" class="container" >
-    <header style="justify-content: center; align-items: center; align-self: center; display: flex; flex-direction: row; flex-direction: row; column-gap: 10px; row-gap: 10px;">
-        <h1 style="color: black;">This is header</h1>
-        <a href="authentication/Login" class="btn btn-primary">Login / Register</a>
-        <a href="Lecturer/overview" class="btn btn-primary">Instructor Dashboard</a>
-    </header>
->>>>>>> af39c53763d08efa883b823a7c125640bda6b4ea
+
+        </div>
     </div>
+
 </html>
     
