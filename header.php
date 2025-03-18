05fcb753b9cca978b8df82157e2621d84165ea96
praveen <praveenisuru06@gmail.com>
2025-03-18 23:01:38 +0530

    header and footer

--- header.php
+++ header.php
@@ -16,16 +16,63 @@
 
     <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
 
-
-
 </head>
+
 <body>
-    <div class="head">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../index.php">                        
+                <img src="assets/images/logo.png" style="height: 48px; margin-left:1rem;">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 navitem">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="course.php">Course</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                   
+                </ul>
+
+            <form class="d-flex">
+                <button class="loginbtn" style="background-color: #f8f9fa; border:none; margin-right: 1rem;">
+                    <a href="authentication/Login" style="color:#0B1623; text-decoration:none; font-weight: bold;">Login</a>
+                </button>
+                <button class="loginbtn" style="margin-right: 1rem;">
+                    <a href="authentication/Login" style="color:white; text-decoration:none;">Register</a>
+                </button>
+            </form>
+            </div>
+        </div>
+    </nav>
+
+
+
+    
+
+
+    <!-- <div class="head">
         <div style="background-color: white;" >
             <header style="display: flex; flex-direction: row; flex-direction: row; column-gap: 10px; row-gap: 10px;">
                 <div class="row w-100">
                     <div class="col-4">
-                        <img class="col-5" src="assets/images/logo.png"></div>
+                        <img class="col-5" src="assets/images/logo.png">
+                    </div>
                     
                     <div class="col-5 menu-bar">
                         <a href="../index.php" class="menu-link">Home</a>
@@ -39,14 +86,13 @@
                     </div>
                 </div>
             </header>
-
         
         <header style="justify-content: center; align-items: center; align-self: center; display: flex; flex-direction: row; flex-direction: row; column-gap: 10px; row-gap: 10px;">
         
         </header>
 
         </div>
-    </div>
+    </div> -->
 </header>
 
 </html>
